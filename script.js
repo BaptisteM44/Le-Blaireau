ab4b3dd70b45c394ee49f35703603f929932949b
BaptisteM44 <bapmorvan@gmail.com>
2022-03-17 09:55:00 +0100

    header/burger/cards

--- script.js
+++ script.js
@@ -4,31 +4,30 @@ navElements = document.getElementById('nav-elements')
 
 window.addEventListener('scroll', function(){
     const scroll = this.document.documentElement.scrollTop;
-
-    console.log(scroll);
     if (scroll > 200){
         navbar.style.height = "8vh";
         navElements.style.height = "8vh";
         navbar.style.background = "rgba(255, 255, 255, 0.589)"
-        menu.style.background = "rgba(255, 255, 255, 0.589)"
-        logo.style.height = "70px"
-        logo.style.width = "70px"
+        navbar__links.style.top = "38px"
     } else {
         navbar.style.height = "11vh";
         navElements.style.height = "11vh";
         navbar.style.background = "rgb(255, 255, 255)"
-        menu.style.background = "rgb(255, 255, 255)"
-        logo.style.height = "110px"
-        logo.style.width = "110px"
+        navbar__links.style.top = "58px"
     }
 
 })
 /* END navbar flow */
-$("#toggle").click(function() {
-  $(this).toggleClass("on");
-  $("#menu").slideToggle();
-});
+/* BURGER */
 
+function toggleMenu () {  
+  const navbar = document.querySelector('.nav-elements');
+  const burger = document.querySelector('.burger');
+  burger.addEventListener('click', (e) => {    
+    navbar.classList.toggle('show-nav');
+  });    
+}
+toggleMenu();
 
 /* Parallax */
 const parallax = document.getElementById("parallax");
