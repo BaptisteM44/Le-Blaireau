be47f8807bb241e33fcbc498fd41941385534237
BaptisteM44 <bapmorvan@gmail.com>
2022-03-18 17:32:50 +0100

    cards

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /* script navbar flow */
 navbar = document.getElementById('navbar');
 navElements = document.getElementById('nav-elements')
+navbarlinks = document.getElementById('navbarlinks')
 
 window.addEventListener('scroll', function(){
     const scroll = this.document.documentElement.scrollTop;
@@ -8,12 +9,12 @@ window.addEventListener('scroll', function(){
         navbar.style.height = "8vh";
         navElements.style.height = "8vh";
         navbar.style.background = "rgba(255, 255, 255, 0.589)"
-        navbar__links.style.top = "38px"
+        navbarlinks.style.top = "65px"
     } else {
         navbar.style.height = "11vh";
         navElements.style.height = "11vh";
         navbar.style.background = "rgb(255, 255, 255)"
-        navbar__links.style.top = "58px"
+        navbarlinks.style.top = "90px"
     }
 
 })
